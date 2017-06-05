57695a71f750dbba9dbfc1dafaddb229fafcd98a
WINDOWS-A65N7P6\renw <ycjay19434@gmail.com>
2017-06-05 10:57:47 +0800

    6-5
    兼容IE8

--- michael_img/js/index.js
+++ michael_img/js/index.js
@@ -1,15 +1,14 @@
-var jQuery = jQuery.noConflict();
-
-jQuery(document).ready(function(){
-	jQuery('.user').mouseover(function(){
-	jQuery('.ui-login-status').css('display','block');
+(function($){
+	$(document).ready(function(){
+		$('.user').mouseover(function(){
+	$('.ui-login-status').css('display','block');
 	});
-	jQuery('.user').mouseout(function(){	
-		jQuery('.ui-login-status').css('display','none');
+	$('.user').mouseout(function(){	
+		$('.ui-login-status').css('display','none');
 	});
 	//折叠
-	jQuery('.btn-fold').on('click',function(e){
-		jQuery(e.target).parents('.foldableBox').toggleClass('fold');
+	$('.btn-fold').click(function(e){
+		$(e.target).parents('.foldableBox').toggleClass('fold');
 	});
-
-});
+	});
+})(jQuery);
